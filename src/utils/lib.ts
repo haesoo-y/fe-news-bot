f9b919cf0ced4cebac0d260a7fda54578b280764
Harry <haesoo9410@gmail.com>
2022-04-02 19:41:00 +0900

    :sparkles: 한국 시간 반환 함수 추가

--- src/utils/lib.ts
+++ src/utils/lib.ts
@@ -12,9 +12,26 @@ export const getHtml = async (url: string) => {
 
 export const makeMarkDown = (lst: ObjType[]) => {
   let result = '# 오늘의 포스팅 \n';
-  result += new Date() + '기준 \n';
+  result += getKoreaTime() + ' 기준 \n';
   for (const obj of lst) {
-    result += `### ${obj.title} \n${obj.desc} \n[바로가기](${obj.url}) \n`;
+    result += `### ${obj.title} \n ${obj.desc} \n\n [바로가기](${obj.url}) \n`;
   }
   fs.writeFileSync('README.md', result, 'utf-8');
+};
+
+const getKoreaTime = () => {
+  const now = new Date();
+  const diffConfig = now.getTimezoneOffset() * 60 * 1000;
+  const diffKorea = 9 * 60 * 60 * 1000;
+  const koreaTime = new Date(now.getTime() + diffConfig + diffKorea);
+  return (
+    koreaTime.getFullYear() +
+    '년' +
+    koreaTime.getMonth() +
+    '월' +
+    koreaTime.getDay() +
+    '일' +
+    koreaTime.getHours() +
+    '시'
+  );
 };
