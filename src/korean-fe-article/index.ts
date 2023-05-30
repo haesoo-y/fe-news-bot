b9f51e54d71fda18da090ddec173661374e0d2ae
harry-yoon <harry.yoon@kakaoenterprise.com>
2023-05-30 22:18:59 +0900

    🔧 kfa 크롤링 정상 동작하도록 수정

--- src/korean-fe-article/index.ts
+++ src/korean-fe-article/index.ts
@@ -1,21 +1,31 @@
-import { ObjType } from '@/type';
-import { getHtml } from '@/utils/lib';
-import { MAX_DESC_LENGTH } from '@/utils/static';
-import * as cheerio from 'cheerio';
+import { ObjType } from "@/type";
+import { getHtml } from "@/utils/lib";
+import { MAX_DESC_LENGTH } from "@/utils/static";
+import * as cheerio from "cheerio";
 
 export const getKFAList = async () => {
-  const kfa = 'https://kofearticle.substack.com';
-  const kfaTitle = '[Korean FE Article]';
+  const kfa = "https://kofearticle.substack.com";
+  const kfaTitle = "[Korean FE Article]";
   const result: ObjType[] = [];
   const html = await getHtml(kfa);
   const $ = cheerio.load(html?.data);
-  const $bodyList = $('div.home-page .post-preview-content');
+  const $bodyList = $(
+    "div.portable-archive-list > div > div > div.pencraft > div.pencraft > div.pencraft"
+  );
 
   $bodyList.each((i, elem) => {
     result.push({
-      title: $(elem).find('a.post-preview-title').text().replace(kfaTitle, ''),
-      desc: $(elem).find('a.post-preview-description').text().slice(0, MAX_DESC_LENGTH) + '...',
-      url: '' + $(elem).find('a.post-preview-title').attr('href'),
+      title: $(elem)
+        .find(".pencraft > .pencraft:nth-child(1) a")
+        .text()
+        .replace(kfaTitle, ""),
+      desc:
+        $(elem)
+          .find(".pencraft > .pencraft:nth-child(2) a")
+          .text()
+          .slice(0, MAX_DESC_LENGTH) + "...",
+      url:
+        "" + $(elem).find(".pencraft > .pencraft:nth-child(1) a").attr("href"),
     });
   });
 
