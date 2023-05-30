d3c9633365d6c2e1014e6b0b56adb4f9fc216fc9
harry-yoon <harry.yoon@kakaoenterprise.com>
2023-05-30 22:18:21 +0900

    🎨 ts 파일 포매팅 변경

--- src/yozm/index.ts
+++ src/yozm/index.ts
@@ -1,20 +1,25 @@
-import { ObjType } from '@/type';
-import { getHtml } from '@/utils/lib';
-import { MAX_DESC_LENGTH } from '@/utils/static';
-import * as cheerio from 'cheerio';
+import { ObjType } from "@/type";
+import { getHtml } from "@/utils/lib";
+import { MAX_DESC_LENGTH } from "@/utils/static";
+import * as cheerio from "cheerio";
 
 export const getYozmList = async () => {
-  const yozm = 'https://yozm.wishket.com';
+  const yozm = "https://yozm.wishket.com";
   const result: ObjType[] = [];
-  const html = await getHtml(yozm + '/magazine/list/develop');
+  const html = await getHtml(yozm + "/magazine/list/develop");
   const $ = cheerio.load(html?.data);
-  const $bodyList = $('div.list-cover ').children('div.list-item-link');
+  const $bodyList = $("div.list-cover ").children("div.list-item-link");
 
   $bodyList.each((i, elem) => {
     result.push({
-      title: $(elem).find('.list-item .item-main a.item-title').text(),
-      desc: $(elem).find('.list-item .item-description').text().slice(0, MAX_DESC_LENGTH) + '...',
-      url: yozm + $(elem).find('.list-item .item-main a.item-title').attr('href'),
+      title: $(elem).find(".list-item .item-main a.item-title").text(),
+      desc:
+        $(elem)
+          .find(".list-item .item-description")
+          .text()
+          .slice(0, MAX_DESC_LENGTH) + "...",
+      url:
+        yozm + $(elem).find(".list-item .item-main a.item-title").attr("href"),
     });
   });
   return result;
